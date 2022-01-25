536e8d920babcdf13b54f8361a5821e02d641812
angelr08 <a4423092@hotmail.com>
2022-01-25 19:32:31 -0400

    Culminación del conversor bolívares-dolares

---
+++ dolaresBolivares.js
@@ -0,0 +1,16 @@
+const bolivar = 4.75;
+function convertirBolivares() {
+    const dolares = document.getElementById("dinero");
+    const value = dolares.value;
+    const resultado = ((value * 1) / bolivar).toFixed(2);
+    
+    const resultadoTexto = document.getElementById("resultadoDolares");
+    resultadoTexto.innerText = "El valor en dolares es de: $" + resultado;
+}
+function convertirDolares() {
+    const bolivares = document.getElementById("dinero");
+    const value = bolivares.value;
+    const resultado = (value * bolivar).toFixed(2);
+    const resultadoTexto = document.getElementById("resultadoDolares");
+    resultadoTexto.innerText = "El valor en bolivares es de: Bs." + resultado;
+}
